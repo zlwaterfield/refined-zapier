21f433a48a8a515e4e93afb7315d82c072e69d2d
Jishnu Mohan <mohanjishnu@gmail.com>
2021-05-02 09:24:09 +0800

    Cache Zap details when zaps are ready on dashboard page load

--- source/features/show-zap-details-on-hover.tsx
+++ source/features/show-zap-details-on-hover.tsx
@@ -1,14 +1,15 @@
 import React from 'dom-chef';
 import delegate from 'delegate-it';
+import elementReady from 'element-ready';
 import select from 'select-dom';
 
 import features from '.';
 import './show-zap-details-on-hover.css';
 import {isZaps} from '../helpers/page-detect';
-import {onDashboardZapHover} from '../events/on-div-hover';
+import {onDashboardZapIconsHover} from '../events/on-div-hover';
 import { fetchZapDetails } from '../helpers/api';
 
-async function handleZapHover(event: delegate.Event<MouseEvent>): Promise<void> {
+async function handleZapIconsHover(event: delegate.Event<MouseEvent>): Promise<void> {
 	const zapIconsDiv = event.delegateTarget;
 	const zapIconsDivWrapper = zapIconsDiv.parentElement?.parentElement?.parentElement;
 	const zapId = zapIconsDivWrapper!.getAttribute('data-zap-id');
@@ -18,6 +19,7 @@ async function handleZapHover(event: delegate.Event<MouseEvent>): Promise<void> 
 	}
 
 	if (event.type === 'mouseover') {
+        console.log(event);
 		const overview = await fetchZapDetails(zapId);
 		const existingTooltip = select(`#icon-tooltip-${zapId}`);
 		if (existingTooltip) {
@@ -34,13 +36,25 @@ async function handleZapHover(event: delegate.Event<MouseEvent>): Promise<void> 
 			);
 		}
 	} else if (event.type === 'mouseout') {
+        console.log(event);
 		select(`#icon-tooltip-${zapId}`)?.classList.add('hide-tooltip');
 	}
 }
 
+async function cacheTooltips(): Promise<void> {
+    await elementReady('.zap-mini', {'stopOnDomReady': false});
+
+    console.log('Zaps seem to be loaded, try to to pre-warm cache');
+    const zapIds = select.all('.zap-mini').map(el => el!.getAttribute('data-zap-id'));
+    // Fire API requests to load the necessary data. 
+    // These are memoized in session so loads on hover are fast.
+    zapIds.filter(zapId => zapId !== null).map(zapId => fetchZapDetails(zapId!));
+}
+
 async function init(): Promise<false | void> {
-	onDashboardZapHover(handleZapHover);
-	// Pre-cache the hover information
+	onDashboardZapIconsHover(handleZapIconsHover);
+
+    cacheTooltips();
 }
 
 void features.add(__filebasename, {
