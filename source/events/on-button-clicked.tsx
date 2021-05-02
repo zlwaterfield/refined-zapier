84a9da9363a337de2b0981419afca9265061ae2e
Zach Waterfield <zlwaterfield@gmail.com>
2021-05-02 13:22:20 -0400

    Remove unused event file

--- source/events/on-button-clicked.tsx
+++
@@ -1,9 +0,0 @@
-import delegate from 'delegate-it';
-
-type DelegateFieldEvent = delegate.EventHandler<MouseEvent, HTMLButtonElement>;
-
-function onButtonClicked(selector: string, callback: DelegateFieldEvent): void {
-  delegate<HTMLButtonElement, 'click'>(document, selector, 'click', event => {
-    callback(event);
-  });
-}
